378ef9e72313b79e46c722673c9ae8445c251697
Alexander Niebuhr <alexander.niebuhr@fricke.de>
2019-08-30 18:42:31 +0200

    MP Rooms
    - improved network lookup
    - added libs

--- src/js/client.js
+++ src/js/client.js
@@ -1,33 +1,66 @@
 const ColyseusClient = require('colyseus.js')
+const ip = require('ip')
 let client
 
-async function joinRoom() {  
-  
-}
+async function joinRoom() {}
 
-async function connectServer() {
-  let remoteIP = document.querySelector('#remoteIPAddress').value
-  console.log(remoteIP)
-  client = await new ColyseusClient.Client(`ws://${remoteIP}:2567`);
-  console.log(client);
-  
-}
+async function findRooms() {
+  const isPortReachable = require('is-port-reachable')
 
-async function getAvailableRooms() {
-  client.getAvailableRooms("battle").then(rooms => {
-    rooms.forEach((room) => {
-      console.log(room.roomId);
-      console.log(room.clients);
-      console.log(room.maxClients);
-      console.log(room.metadata);
-      document.querySelector('#roomList').innerHTML = "<li>" + room.roomId + "</li>"
-    });
-  }).catch(e => {
-    console.error(e);
-  });
+  const LANScanner = require('lanscanner')
+  let network = await LANScanner.scan()
+  console.log(network)
+  networkIPs = network.map(device => device.ip)
+  networkIPs.push(ip.address())
+  console.log(networkIPs)
+
+  for (const deviceIP of networkIPs) {
+    console.log(deviceIP)
+    const wsPing = await isPortReachable(2567, { host: deviceIP })
+    console.log(wsPing)
+
+    if (wsPing === true) {
+      try {
+        const connection = new ColyseusClient.Client(`ws://${deviceIP}:2567`)
+        connection
+          .getAvailableRooms('battle')
+          .then(rooms => {
+            console.log(rooms)
+          })
+          .catch(err => {
+            console.error(err)
+          })
+      } catch (err) {
+        console.error(err)
+      }
+    } else {
+    }
+
+    // const connection = new ColyseusClient.Client(`ws://${deviceIP}:2567`)
+
+    // connection.getAvailableRooms('battle').then(rooms => {
+    //   console.log(rooms)
+    // }).catch(err => {
+    //   // console.log(err)
+    // })
+  }
+  // for (const ipAddress of networkIPs) {
+  //   console.log(ipAddress)
+  //   client = new ColyseusClient.Client(`ws://${ipAddress}:2567`)
+  //   let rooms = await client.getAvailableRooms('battle')
+  //   console.log(rooms)
+
+  //   // rooms.forEach(room => {
+  //   //   console.log(room.roomId)
+  //   //   console.log(room.clients)
+  //   //   console.log(room.maxClients)
+  //   //   console.log(room.metadata)
+  //   //   li += '<li>' + ipAddress + ' - ' + room.roomId + '</li>'
+  //   // })
+  // }
+  // document.querySelector('#roomList').innerHTML = li
 }
 
 document.querySelector('#findRooms').addEventListener('click', async () => {
-  await connectServer()
-  await getAvailableRooms()
+  await findRooms()
 })
